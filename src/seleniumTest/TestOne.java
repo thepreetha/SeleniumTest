ca77c964c9b19bf575f63e8909defa5b1aedf605
preetha <preetha.mathiazhagan@outlook.com>
2025-07-05 23:23:41 -0700

    First commit

---
+++ src/seleniumTest/TestOne.java
@@ -0,0 +1,13 @@
+package seleniumTest;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
